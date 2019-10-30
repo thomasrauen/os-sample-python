faef72b6daeefa95a0bcb7b0bf5588e0866ed3b0
thomasrauen <thomas.rauen@rohde-schwarz.com>
2019-10-30 20:06:31 +0100

    ID Generator

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,13 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Again!"
+    id_file = open("id.txt", "r")
+    id = id_file.read()
+    id_file.close()
+    new_id = int(id) + 1
+    id_file = open("id.txt", "w")
+    id_file.write(str(id))
+    return id
 
 if __name__ == "__main__":
     application.run()
