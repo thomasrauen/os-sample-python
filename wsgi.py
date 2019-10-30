57825fd5fa10017e707c221abdfa3cbc684fea14
thomasrauen <thomas.rauen@rohde-schwarz.com>
2019-10-30 21:55:11 +0100

    id-gen

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+id = 1
+def id_generator():
+    id = int(id) + 1
+    return str(id)
 
 if __name__ == "__main__":
     app.run()
