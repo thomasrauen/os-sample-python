c66be0a65f36a58c0190b1282a077daa0161ea2f
thomasrauen <thomas.rauen@rohde-schwarz.com>
2019-10-30 22:07:52 +0100

    reset

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def id_generator():
-    id1 = 25
-    return str(id1)
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run()
